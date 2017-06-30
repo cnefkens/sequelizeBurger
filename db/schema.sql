6fb49aeed68546ab071709ea71b12f9ba43653a8
Chuck Nefkens <nefkens@speakeasy.net>
2017-06-30 16:43:57 -0700

    fix schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ use burger_db;
 create table burgers (
 id integer(11) auto_increment not null
 ,burger_name varchar(50) not null
-,devoured bool default 0
+,devoured boolean default 0
 ,createdAt timestamp default CURRENT_TIMESTAMP not null
 ,Primary Key (id)
 );
